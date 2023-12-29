211366eeaebf77df8d884febe6ac02c0d99861cc
Fernando Rivera <58616200+FerRiv3ra@users.noreply.github.com>
2023-12-29 18:34:51 +0000

    Icons pack ready

--- src/navigator/DrawerNavigator.tsx
+++ src/navigator/DrawerNavigator.tsx
@@ -1,11 +1,14 @@
+import {Text, TouchableOpacity, View} from 'react-native';
 import {
   DrawerContentComponentProps,
   DrawerContentScrollView,
   createDrawerNavigator,
 } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import {StackNavigator} from './StackNavigator';
 import {Settings} from '../screens/SettingsScreen';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {drawerStyles} from '../theme/drawerStyles';
 
 export type RootDrawerParams = {
   Home: undefined;
@@ -31,20 +34,26 @@ const DrawerContent = ({navigation}: DrawerContentComponentProps) => {
       style={{
         backgroundColor: '#00A294',
       }}>
+      <View style={{...drawerStyles.icon}}>
+        <Icon name="list-circle-outline" color="#FFF" size={60} />
+        <Text style={{...drawerStyles.txtButton}}>TODO APP</Text>
+      </View>
       <View>
-        <View>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('Home')}
-            activeOpacity={0.6}>
-            <Text>Home</Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={{...drawerStyles.button}}
+          activeOpacity={0.6}>
+          <Icon name="home" size={17} color="#FFF" />
+          <Text style={{...drawerStyles.txtButton}}>Home</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={() => navigation.navigate('Settings')}
-            activeOpacity={0.6}>
-            <Text>Settings</Text>
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Settings')}
+          style={{...drawerStyles.button}}
+          activeOpacity={0.6}>
+          <Icon name="cog" size={17} color="#FFF" />
+          <Text style={{...drawerStyles.txtButton}}>Settings</Text>
+        </TouchableOpacity>
       </View>
     </DrawerContentScrollView>
   );
---
+++ src/theme/drawerStyles.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const drawerStyles = StyleSheet.create({
+  icon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  txtButton: {
+    color: '#FFF',
+    marginLeft: 5,
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
